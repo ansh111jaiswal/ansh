8483362382b8cc4900a7aaaa39daeffec969d925
ansh111jaiswal <ansh111jaiswal@gmail.com>
2026-08-12 20:47:25 +0530

    ZigZag Pattern W

---
+++ zigzag-pattern-w.py
@@ -0,0 +1,10 @@
+rows = 3
+cols = 9
+
+for i in range(rows):
+    for j in range(cols):
+        if (i + j) % 4 == 0 or (i == 1 and j % 4 == 2):
+            print("*", end="")
+        else:
+            print(" ", end="")
+    print()
