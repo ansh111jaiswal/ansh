e6c7802850fd07abff9a67532cc10077a544c2e9
ansh111jaiswal <ansh111jaiswal@gmail.com>
2026-08-14 22:04:03 +0530

    Snake Matrix Pattern

---
+++ snake-matrix.py
@@ -0,0 +1,18 @@
+n = 4
+num = 1
+
+for i in range(n):
+
+    if i % 2 == 0:
+        for j in range(n):
+            print(num, end=" ")
+            num += 1
+    else:
+        start = num + n - 1
+
+        for j in range(n):
+            print(start - j, end=" ")
+
+        num += n
+
+    print()
