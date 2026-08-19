46f064b0e226646f8ed1cadb688a5682b3757452
ansh111jaiswal <ansh111jaiswal@gmail.com>
2026-08-19 21:13:26 +0530

    Number Cross Pattern

---
+++ number-cross.py
@@ -0,0 +1,9 @@
+n = 5
+
+for i in range(n):
+    for j in range(n):
+        if i == j or i + j == n - 1:
+            print(i + 1, end=" ")
+        else:
+            print(" ", end=" ")
+    print()
