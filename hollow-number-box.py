41ef0e4e0036db3936cee86b8455d8005bb0900d
ansh111jaiswal <ansh111jaiswal@gmail.com>
2026-08-26 19:44:53 +0530

    Hollow Number Box Pattern

---
+++ hollow-number-box.py
@@ -0,0 +1,17 @@
+n = 5
+
+for i in range(n):
+    for j in range(n):
+
+        if i == 0:
+            print(j + 1, end="")
+        elif i == n - 1:
+            print(n - j, end="")
+        elif j == 0:
+            print(i + 1, end="")
+        elif j == n - 1:
+            print(n - i, end="")
+        else:
+            print(" ", end="")
+
+    print()
